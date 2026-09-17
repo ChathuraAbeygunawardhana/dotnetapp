0b4a5734289f628f2e9f3f5de86e945e89d4a7ca
ChathuraAbeygunawardhana <chathuraabeygunawardhana@gmail.com>
2026-09-18 00:19:27 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+var builder = WebApplication.CreateSlimBuilder(args);
+var port = Environment.GetEnvironmentVariable("PORT") ?? "80";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
+var app = builder.Build();
+
+app.MapGet("/", () => Results.Json(new { message = "Hello from .NET on Vercel" }));
+app.MapGet("/health", () => Results.Json(new { status = "ok" }));
+
+app.Run();
